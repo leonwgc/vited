fbda5e64dfbe054571560b1e08401c617d43fbd4
leonwgc <giantfish@126.com>
2021-05-15 22:25:03 +0800

    chore: update

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,8 @@ var _require = require('commander'),
 
 var start = require('./dev');
 
-var program = new Command();
+var program = new Command(); // config dir name under src  or entry filename(no ext) under src
+
 program.version('0.0.1');
 program.option('-c, --config <dir>', '包含入口文件index.jsx/tsx的目录名').option('-b, --build', '打包编译到dist');
 program.parse(process.argv);
